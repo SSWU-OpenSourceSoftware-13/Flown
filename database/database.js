71b1e6db7ce6f7e9e399829c0f4562f0c8ff87b8
Han seyoung <20201016@sungshin.ac.kr>
2023-12-01 21:27:26 +0900

    #1 : dockerfile과 docker-compose 파일 생성 및 작성

--- database/database.js
+++ database/database.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const options = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PW,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
+    host: 'mysql-container2',
+    user: 'root',
+    password: 'tjdtls!',
+    port: '3307',
+    database: 'flown'
 }
 
 const db = mysql.createPool(options)
